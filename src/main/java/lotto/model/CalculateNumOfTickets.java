394105627d256ee4d37463bef9cd1919f98f9fd3
hyebin <shin378378@naver.com>
2024-10-09 04:58:44 +0900

    <STYLE> CalculateNumOfTickets 선언 순서 변경

--- src/main/java/lotto/model/CalculateNumOfTickets.java
+++ src/main/java/lotto/model/CalculateNumOfTickets.java
@@ -13,12 +13,11 @@ public class CalculateNumOfTickets {
         this.purchasePrice = purchasePrice;
     }
 
-    public int getNumOfTickets() {
-        return numOfTickets;
-    }
-
-    public int getPurchasePrice() {
-        return purchasePrice;
+    //티켓 개수 정하기
+    public void settingNumOfTickets(int purchasePrice) {
+        purchasePriceException(purchasePrice);
+        this.purchasePrice=purchasePrice;
+        this.numOfTickets = purchasePrice/TICKET_PRICE;
     }
 
     //예외 - 구매금액이 양수가 아닌 경우
@@ -28,10 +27,11 @@ public class CalculateNumOfTickets {
         }
     }
 
-    //티켓 개수 정하기
-    public void settingNumOfTickets(int purchasePrice) {
-        purchasePriceException(purchasePrice);
-        this.purchasePrice=purchasePrice;
-        this.numOfTickets = purchasePrice/TICKET_PRICE;
+    public int getNumOfTickets() {
+        return numOfTickets;
+    }
+
+    public int getPurchasePrice() {
+        return purchasePrice;
     }
 }
